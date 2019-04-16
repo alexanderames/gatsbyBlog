2cd68de7129b9bad46c39f62719b25168f7213a3
alexander ames <alexanderthomasames@gmail.com>
2019-04-16 11:11:05 -0600

    add link cmp

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,12 @@
 import React from "react"
+import { Link } from "gatsby"
 
 export default () => (
   <div>
     <h1>This is the index page</h1>
+    <div>
+      <Link to="/">HOME</Link> | <Link to="/about">ABOUT</Link>
+    </div>
     <p>
     Lorem ipsum and all that fun shit.
     </p>
