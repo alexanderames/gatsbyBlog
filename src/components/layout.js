c7787a12b7bcd571d8cf1b3d01cb8d430c17ed0b
alexander ames <alexanderthomasames@gmail.com>
2019-05-04 07:15:08 -0600

    create header, footer and title

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,10 +1,16 @@
 import React from 'react'
 import styles from './layout.module.scss'
 import Header from './header.js'
+import Footer from './footer.js'
 
 export default ({children}) => (
   <div className={styles.container}>
-    <Header/>
-    {children}
+     <Header />
+     <div className={styles.content}>
+        {children}
+     </div>
+     <Footer>
+        ÄMęŚ
+   </Footer>
   </div>
 )
